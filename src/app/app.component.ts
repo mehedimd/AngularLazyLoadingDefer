efdf23508da12b3a6683c629771923f7fba8058e
mehedimd <mehedi105662@gmail.com>
2024-07-07 13:56:24 +0600

    Angular 17 Lazy Loading using @Defer @placeholder @loading @error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ItemListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
